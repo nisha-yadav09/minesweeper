838320c151f328f625a043683e2ada6a508ffdc1
nisha09 <nisha1947@gmail.com>
2022-04-02 16:40:42 -0600

    Adding  attribute to table

--- js/main.js
+++ js/main.js
@@ -20,6 +20,7 @@ let arrBoard ;
 let flagCount = null;
 /*----- cached element references -----*/
 
+
 /*----- event listeners -----*/
 
 //document.querySelector()
@@ -30,22 +31,24 @@ let flagCount = null;
 init();
 
 function init() {
+    createBoard(boardChoice);
     createArray(boardChoice);
     fillBoardWithRandomMines(boardChoice);
      
 }
 
-function createBoard(boardChoice)
-{
+function createBoard(boardChoice) {
     let boardRootEl = document.querySelector("div#table-div");
     let table = document.createElement('table');
     boardRootEl.appendChild(table);
     for (let i = 0; i < parseInt(boardChoice); i++) {
         let tr = document.createElement('tr');
         table.appendChild(tr);
-        for (let i = 0; i < parseInt(boardChoice); i++) { 
+        for (let j = 0; j < parseInt(boardChoice); j++) { 
             let td = document.createElement('td');
             tr.appendChild(td);
+            td.setAttribute("x-index" , i);
+            td.setAttribute("y-index" , j);
         }
     }  
 }
@@ -53,7 +56,6 @@ function createBoard(boardChoice)
 function getRandomMinesIndex(boardChoice, idx) {
     const rndXIdx = Math.floor(Math.random() * (boardChoice-1));
     const rndYIdx = Math.floor(Math.random() * (boardChoice-1));
-    console.log("Index" + rndXIdx, rndYIdx );
     return idx === 'x' ? rndXIdx : rndYIdx;
 }
 
@@ -80,6 +82,6 @@ function fillBoardWithRandomMines(boardChoice) {
         let yMine = getRandomMinesIndex(boardChoice, "y");
         arrBoard[xMine][yMine].value = arrBoard[xMine][yMine].value === "mine" ? "" : "mine";
         arrBoard[xMine][yMine].value === "mine" ? tempMineChoice -- : tempMineChoice;
-        console.log("Array" + arrBoard );
     } while (tempMineChoice>=1);
 }
+
