bb4117d35a787c06b557466c92bd352109a17fb8
nisha09 <nisha1947@gmail.com>
2022-04-02 14:25:43 -0600

    Added board functions

--- js/main.js
+++ js/main.js
@@ -1,29 +1,40 @@
 /*----- constants -----*/
 
-const boardSize = {beginner:"8", 
-                intermediate: "16" ,
-                expert:"24"};
-const mines = {beginner :10, intermediate: 40 ,expert:99};
+const boardSize = {
+    beginner:"8", 
+    intermediate: "16" ,
+    expert:"24"
+};
+const mines = {
+    beginner :10, 
+    intermediate: 40,
+    expert:99
+};
+let boardChoice =  boardSize.beginner;
+let mineChoice =  mines.beginner;
 
 
 /*----- app's state (variables) -----*/
 
-let arrBoard = [];
-arrBoard = [
-    [{value}],
-    []
-]
+let arrBoard ;
 let flagCount = null;
 /*----- cached element references -----*/
 
 /*----- event listeners -----*/
 
+//document.querySelector()
+
 //const difficultyDropboxEl = document. 
 
 /*----- functions -----*/
+init();
 
-let boardChoice = boardSize.beginner;
-createBoard(boardChoice);
+function init() {
+    createArray(boardChoice);
+    fillBoardWithRandomMines(boardChoice);
+     
+}
+
 function createBoard(boardChoice)
 {
     let boardRootEl = document.querySelector("div#table-div");
@@ -36,7 +47,39 @@ function createBoard(boardChoice)
             let td = document.createElement('td');
             tr.appendChild(td);
         }
-    }
+    }  
+}
 
-    
+function getRandomMinesIndex(boardChoice, idx) {
+    const rndXIdx = Math.floor(Math.random() * (boardChoice-1));
+    const rndYIdx = Math.floor(Math.random() * (boardChoice-1));
+    console.log("Index" + rndXIdx, rndYIdx );
+    return idx === 'x' ? rndXIdx : rndYIdx;
+}
+
+function createArray(boardChoice) {
+    arrBoard = new Array(boardChoice);
+    for (let i = 0; i < parseInt(boardChoice); i++) {
+        arrBoard[i] = [];
+    }
+    for(let i = 0; i < arrBoard.length; i++) {
+        for(let j = 0; j < arrBoard.length; j++) {
+            arrBoard[i][j] = {
+                value: "",
+                hasFlag: false,
+                reveal: false
+            }
+        }
+    }
+}
+
+function fillBoardWithRandomMines(boardChoice) {
+    let tempMineChoice = mineChoice;
+    do {
+        let xMine = getRandomMinesIndex(boardChoice, "x");
+        let yMine = getRandomMinesIndex(boardChoice, "y");
+        arrBoard[xMine][yMine].value = arrBoard[xMine][yMine].value === "mine" ? "" : "mine";
+        arrBoard[xMine][yMine].value === "mine" ? tempMineChoice -- : tempMineChoice;
+        console.log("Array" + arrBoard );
+    } while (tempMineChoice>=1);
 }
